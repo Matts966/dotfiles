3ba357b8b0b71d00024e0918316324ad1ced9e6c
Matts966 <Matts966@users.noreply.github.com>
2019-08-30 02:21:53 +0900

    Add tmux resurrect

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -2,4 +2,5 @@ mkdir -p .vim/colors
 curl -fLo .vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
+git clone https://github.com/tmux-plugins/tpm .tmux/plugins/tpm
 
